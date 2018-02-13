525251c33425b4b236b5085b1148069fc6d0f9a6
cckevincyh <835287880@qq.com>
2018-02-13 22:18:35 +0800

    实现bootstrap-Treeview级联勾选

---
+++ src/main/resources/public/static/js/admin/admin/permissionTree.js
@@ -0,0 +1,106 @@
+/**
+ * bootstrap-Treeview实现级联勾选
+ * Created by c on 2018/2/11.
+ */
+var nodeCheckedSilent = false;
+function nodeChecked (event, node){
+    if(nodeCheckedSilent){
+        return;
+    }
+    nodeCheckedSilent = true;
+    checkAllParent(node);
+    checkAllSon(node);
+    nodeCheckedSilent = false;
+}
+
+var nodeUncheckedSilent = false;
+function nodeUnchecked  (event, node){
+    if(nodeUncheckedSilent)
+        return;
+    nodeUncheckedSilent = true;
+    uncheckAllParent(node);
+    uncheckAllSon(node);
+    nodeUncheckedSilent = false;
+}
+
+//选中全部父节点
+function checkAllParent(node){
+    $('#tree').treeview('checkNode',node.nodeId,{silent:true});
+    var parentNode = $('#tree').treeview('getParent',node.nodeId);
+    if(!("nodeId" in parentNode)){
+        return;
+    }else{
+        checkAllParent(parentNode);
+    }
+}
+//取消全部父节点
+function uncheckAllParent(node){
+    $('#tree').treeview('uncheckNode',node.nodeId,{silent:true});
+    var siblings = $('#tree').treeview('getSiblings', node.nodeId);
+    var parentNode = $('#tree').treeview('getParent',node.nodeId);
+    if(!("nodeId" in parentNode)) {
+        return;
+    }
+    var isAllUnchecked = true;  //是否全部没选中
+    for(var i in siblings){
+        if(siblings[i].state.checked){
+            isAllUnchecked=false;
+            break;
+        }
+    }
+    if(isAllUnchecked){
+        uncheckAllParent(parentNode);
+    }
+
+}
+
+//级联选中所有子节点
+function checkAllSon(node){
+    $('#tree').treeview('checkNode',node.nodeId,{silent:true});
+    if(node.nodes!=null&&node.nodes.length>0){
+        for(var i in node.nodes){
+            checkAllSon(node.nodes[i]);
+        }
+    }
+}
+//级联取消所有子节点
+function uncheckAllSon(node){
+    $('#tree').treeview('uncheckNode',node.nodeId,{silent:true});
+    if(node.nodes!=null&&node.nodes.length>0){
+        for(var i in node.nodes){
+            uncheckAllSon(node.nodes[i]);
+        }
+    }
+}
+
+
+function updatePermission(adminId) {
+    $.ajax({
+        type: "POST",
+        url: "getAdminPermission",
+        dataType: "json",
+        //async: false,
+        data: {
+            adminId : adminId
+        },
+        success: function (result) {
+            $('#tree').treeview({
+                data: result,         // 数据源
+                showCheckbox: true,   //是否显示复选框
+                highlightSelected: true,    //是否高亮选中
+                //nodeIcon: 'glyphicon glyphicon-user',    //节点上的图标
+                //nodeIcon: 'glyphicon glyphicon-globe',
+                //emptyIcon: '',    //没有子节点的节点图标
+                //multiSelect: false,    //多选
+                onNodeChecked:nodeChecked ,
+                onNodeUnchecked:nodeUnchecked
+            });
+        },
+        error: function () {
+            showInfo1("树形权限结构加载失败！")
+        }
+    });
+}
+
+
+
