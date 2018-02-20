1a209143b0a741808c9eac702f8699eb5cf49e1b
cckevincyh <835287880@qq.com>
2018-02-20 15:51:22 +0800

    使用外部tomcat

--- src/main/java/com/ciyou/edu/app/App.groovy
+++ src/main/java/com/ciyou/edu/app/App.groovy
@@ -3,6 +3,8 @@ package com.ciyou.edu.app
 import org.mybatis.spring.annotation.MapperScan
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.web.support.SpringBootServletInitializer
 import org.springframework.context.annotation.ComponentScan
 
 /**
@@ -13,7 +15,14 @@ import org.springframework.context.annotation.ComponentScan
 @SpringBootApplication
 @ComponentScan(basePackages = ["com.ciyou.edu.controller","com.ciyou.edu.aop","com.ciyou.edu.service","com.ciyou.edu.config"])
 @MapperScan(basePackages = "com.ciyou.edu.mapper")
-class App {
+class App extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // 注意这里要指向原先用main方法执行的App启动类
+        return builder.sources(App.class)
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(App.class, args)
     }
